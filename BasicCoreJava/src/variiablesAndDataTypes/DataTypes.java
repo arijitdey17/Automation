bce24662076a2cc9a4dd7890d1f1a4baeb441940
arijitdey17 <arijitdey17@gmail.com>
2020-11-30 14:23:16 +0530

    First time commiting on git

---
+++ BasicCoreJava/src/variiablesAndDataTypes/DataTypes.java
@@ -0,0 +1,23 @@
+package variiablesAndDataTypes;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int age=31;
+		String name="Arijit";
+		char gender='M';
+		boolean marritalStatus=true;
+		double height=5.7;
+		
+		System.out.println(age);
+		
+		//reserved keywords
+		//difference between byte, short, int, long
+		//difference between double and float
+		//JDK, JRE and JVM
+
+	}
+
+}
